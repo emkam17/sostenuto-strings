35ecd620f2242868142e9296de71ea702e9246f2
Emily Kam <etk48@cornell.edu>
2021-08-18 14:53:38 -0700

    mobile edits

--- scripts/scripts-hamburger.js
+++ scripts/scripts-hamburger.js
@@ -1,8 +1,15 @@
 function myFunction() {
-  var x = document.getElementById("navLinks");
-  if (x.style.display === "block") {
+  var x = document.getElementById("navLinksL");
+  var y = document.getElementById("navLinksR");
+  if (x.style.display === "block" && y.style.display =="block") {
     x.style.display = "none";
+    y.style.display = "none";
+    console.log("if statement L")
   } else {
     x.style.display = "block";
+    y.style.display = "block";
   }
 }
+
+
+// Source: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_mobile_navbar
